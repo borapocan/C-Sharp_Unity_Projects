c3d0de7691f8673d5761159d197be3267e0a2eb7
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2020-09-14 10:58:41 +0300

    Add files via upload

---
+++ NumberWizard.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    int max = 1000;
+
+    int min = 1;
+
+    int guess = 500;
+
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        Debug.Log("Welcome to number wizard.");
+        // print("Welcome to number wizard...");
+
+        Debug.Log("Pick a number, don't tell me what it is...");
+
+        Debug.Log("The highest number you can pick is :" + max);
+
+        Debug.Log("The lowest number you can pick is :" + min);
+
+        Debug.Log("Tell me if your number is higher or lower than :" + guess);
+
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+
+        max++;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Detect when the up arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+           // Debug.Log("Up Arrow key was pressed.");
+
+            min = guess;
+
+            NextGuess();
+
+        }
+        //Detect when the down arrow key is pressed down
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+           
+           // Debug.Log("Down Arrow key was pressed.");
+
+            max = guess;
+
+            NextGuess();
+
+        }
+        //Detect when the enter key is pressed down
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+
+            Debug.Log("I am a genius :)");
+        }
+
+    }
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+
+        Debug.Log("Is it higher or lower than " + guess);
+    }
+
+}
