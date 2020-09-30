de210c3111e64aa358bcd7bc19cfd8c580d356b1
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2020-09-30 17:57:04 +0300

    Add files via upload
    Block Breaker

---
+++ Scripts/Block.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    //config parameters
+
+    [SerializeField] AudioClip breakSound;
+
+    [SerializeField] GameObject blockSparklesVFX;
+
+    //[SerializeField] int maxHits;
+
+    [SerializeField] Sprite[] hitSprites;
+
+    //cached references
+
+    Level level;
+
+    //state variables
+
+    [SerializeField] int timesHit; // TODO only serialized for debug purposes
+
+    private void Start()
+    {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (tag == "Breakable")
+        {
+
+            HandleHit();
+
+        }
+
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+
+        int maxHits = hitSprites.Length + 1;
+
+        if (timesHit >= maxHits)
+        {
+            BlockDestroy();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+
+        if (hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.Log("Block sprite is missing from array." + gameObject.name);
+        }
+    }
+
+    private void BlockDestroy()
+    {
+        PlayBlockDestroySFX();
+
+        Destroy(gameObject);
+
+        level.BlocksDestroyed();
+
+        TriggerSparklesVFX();
+
+        //Debug.Log(collision.gameObject.name); //what destroys
+
+        //Debug.Log(collision.otherCollider.name); //what get destroyed
+    }
+
+    private void PlayBlockDestroySFX()
+    {
+        FindObjectOfType<GameSession>().AddToScore();
+
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+
+        Destroy(sparkles, 1f);
+    }
+}
---
+++ Scripts/Level.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    //parameters
+    [SerializeField] int breakableBlocks;
+
+    //cached references
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+
+    public void CountBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void BlocksDestroyed()
+    {
+        breakableBlocks--;
+
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+}
